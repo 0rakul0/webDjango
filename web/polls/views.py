e35835798dd3642f5a240439b4799b05cc167ec9
Jefferson dos Anjos <0rakul0render@gmail.com>
2022-01-25 21:41:18 -0300

    criação das paginas

--- web/polls/views.py
+++ web/polls/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from .models import Question
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse ("olá")
 
-def detail(request, question_id):
-    return HttpResponse('Essta é a pergunta {}'.format(question_id))
+def index(request):
+    lista_questoes_criadas = Question.objects.order_by('pub_date')[:5]
+    context = {'lista_questoes_criadas': lista_questoes_criadas}
+    return render(request, 'polls/index.html', context)
 
 def results(request, question_id):
-    return HttpResponse ('A resposta correta é: {} '.format(question_id))
+    question = Question(pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse ('Seu voto é para para a pergunta {}'.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selecionar_mudanca = question.mudanca_set.get(pk=request.POST['mudanca'])
+    except KeyError:
+        return render(request, 'polls:vote.html',{
+            'question':question,
+            'erro_message':"não foi possivel achar seu voto.",
+        })
+    else:
+        selecionar_mudanca.voto +=1
+        selecionar_mudanca.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
